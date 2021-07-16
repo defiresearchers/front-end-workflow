b39cf819b7e7e4089e65360ad7a4a32c99d71a7a
DeFi Researcher <defiresearchers@gmail.com>
2021-07-16 23:15:05 +0200

    Test(coverage): add code coverage

--- test/stack.test.js
+++ test/stack.test.js
@@ -1,14 +1,47 @@
+function log(value) {
+  console.log("\n\n____LOG:", value)
+}
+
 class Stack {
-    constructor() {
-        this.top = -1
-    }
+  constructor() {
+    this.top = -1;
+    this.items = {};
+  }
+  get peek() {
+    return this.items[this.top]
+  }
+  push(value) {
+    this.top += 1
+    this.items[this.top] = value
+  }
+  pop() {
+    this.items[this.top] = undefined;
+    this.top -= 1
+  }
 }
 
 describe("My Stack", () => {
+
+  let stack;
+  beforeEach(() => {
+    stack = new Stack();
+  })
+
   it("is created empty", () => {
-      const stack = new Stack()
-      expect(stack.top).toBe(-1)
+    expect(stack.top).toBe(-1);
+    expect(stack.items).toEqual({});
   });
-  it.todo("can push to the top");
-  it.todo("can pop");
+  it("can push to the top", () => {
+    stack.push('item')
+    expect(stack.top).toBe(0)
+    expect(stack.peek).toEqual('item')
+  });
+  it("can pop", () => {
+    stack.push('item1');
+    stack.push('item2');
+    stack.push('item3');
+    stack.pop()
+    expect(stack.top).toBe(1)
+    expect(stack.peek).toEqual('item2')
+  });
 });
